7c4f3fbf33d9f5486504a5f31697b02d51f40f99
Anass1599 <anassmoujahid@hotmail.fr>
2022-02-10 23:12:33 +0100

    product2

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -27,6 +27,20 @@ class Cart
 
     }
 
+    public  function decrease($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if($cart[$id] > 1) {
+            $cart[$id]--;
+        }else {
+            unset($cart[$id]);
+        }
+
+        $this->session->set('cart', $cart);
+
+    }
+
     public function get()
     {
         return $this->session->get('cart');
@@ -35,6 +49,15 @@ class Cart
     public  function remove()
     {
         return $this->session->remove('cart');
+    }
+
+    public  function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart', $cart);
     }
 
 }
